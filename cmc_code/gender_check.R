f11fedf690f795c6e85e39e5ee7e08e4478a4c99
kkdang <kristen.dang@sagebase.org>
2014-04-25 13:23:44 -0700

    Added explanation of awk/sed steps; changed to match current colnames of metadata.

--- cmc_code/gender_check.R
+++ cmc_code/gender_check.R
@@ -13,12 +13,16 @@ grep(pattern="Total", x=colnames(merged_report))
 colnames(merged_report)[grep(pattern="Total", x=colnames(merged_report))]
 head(merged_report)
 
-# read in counts on Y and file names in separate files and combine
-Ycounts = read.delim('~/Computing/commonmind/data/Ychrom_counts.out', head = F)
+# Make two input files from the Ychrom_counts.out file: one with counts and one with file names.
+# $ awk '$1 !~ /[1-9]/' Ychrom_counts.txt | sed -e 's/samtools view -c \/projects\/CommonMind\/data\/FROM_CORE\/Production\/fastQ\/BySample\/tophat209.2.2\/tophat_//' | sed -e "s/'chrY'//" > Ychrom_names.out
+# $ awk '$1 ~ /[1-9]/' Ychrom_counts.txt > Ychrom_data.out
+
+# Read in counts on Y and file names in separate files and combine
+Ycounts = read.delim('~/Computing/commonmind/data/Ychrom_data.out', head = F)
 hist(log(Ycounts[,1]), main = "distribution of chrY alignments", col = "papayawhip", xlab = "log(Y counts)")
 Ynames = read.delim('~/Computing/commonmind/data/Ychrom_names.out', head = F)
 rownames(Ycounts) = matrix(unlist(strsplit(as.character(Ynames[,1]), split="/")),ncol = 2, byrow = T)[,1]
-Ycounts[,2] = merged_report$Total.Reads[match(rownames(Ycounts), merged_report$Sample.RNA.ID)]
+Ycounts[,2] = merged_report$DLPFC_RNA_report..Total.Reads[match(rownames(Ycounts), merged_report$DLPFC_RNA_isolation..Sample.RNA.ID)]
 
 # calculated alignment fraction
 Ycounts[,3] = log(Ycounts[,1] / Ycounts[,2])
@@ -40,9 +44,9 @@ Ycounts$calc_gender = as.factor(Ycounts$calc_gender)
 # add gender from metadata
 grep(pattern="Gender", x=colnames(merged_report))
 head(merged_report[,5])
-Ycounts[,5] = merged_report$Gender[match(rownames(Ycounts), merged_report$Sample.RNA.ID)]
+Ycounts[,5] = merged_report$Gender[match(rownames(Ycounts), merged_report$DLPFC_RNA_isolation..Sample.RNA.ID)]
 colnames(Ycounts) = c("Y_counts", "total_reads", "Y_fraction", "calc_gender", "reported_gender")
-levels(Ycounts$calc_gender) = levels(Ycounts$reported_gender)
+levels(Ycounts$reported_gender) = levels(Ycounts$calc_gender) 
 
 # compare gender assignments
 diffGender = Ycounts[,4] != Ycounts[,5]
