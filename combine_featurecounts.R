599d74f16e6fd149d21a5fcd03b472ad800d6727
kkdang <kristen.dang@sagebase.org>
2015-01-15 13:47:47 -0800

    Initiated from outside the COUNTS dir, also shortens samplenames

--- combine_featurecounts.R
+++ combine_featurecounts.R
@@ -1,17 +1,21 @@
-datafiles = dir()
+datafiles = dir('COUNTfeaturecounts')
 datafiles_nosummary = datafiles[grep("summary", datafiles, invert = TRUE)]
 summaryfiles = datafiles[grep("summary", datafiles)]
 
 readFC=function(inFile){
   print(inFile)
-  data = read.delim(inFile, header = TRUE, skip = 1, row.names =1)
+  data = read.delim(paste("COUNTfeaturecounts",inFile,sep="/"), header = TRUE, skip = 1, row.names =1)
   if (ncol(data) > 5) { return(data[,6]) }
   else {return(rep(NA,nrow(data))) }
 }
 
 counts = sapply(datafiles_nosummary,readFC)
-data = read.delim(datafiles_nosummary[1], header = TRUE, row.names =1, skip = 1)
+data = read.delim(paste("COUNTfeaturecounts",datafiles_nosummary[1],sep="/"), header = TRUE, row.names =1)
 rownames(counts) = rownames(data)
+
+zz = as.list(colnames(counts))
+b = sapply(zz,function(x){unlist(strsplit(as.character(x), split = "[.]"))[1]})
+colnames(counts) = b
 write.csv(t(counts), file="featurecounts_readcounts.csv", quote = FALSE)
 
 
