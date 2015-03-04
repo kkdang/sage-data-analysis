f58c112ba79a3b462888f69f4fd831756050a549
kkdang <kristen.dang@sagebase.org>
2015-03-04 12:48:18 -0800

    Separated metadata from data processing; added log10pval heatmap.

--- scri_cran/cranio_common_routines.R
+++ scri_cran/cranio_common_routines.R
@@ -34,35 +34,18 @@ cleanUpDates=function(inDateData){
 
 
 #Function to generate data object
-generateDataAndClinicalObj=function(synId){
+generateDataObjDeprecated=function(synId){
   dataEntity = synGet(synId) # estimated Reads
   library('R.utils')
   gunzip(getFileLocation(dataEntity),overwrite=TRUE)
   x = unlist(strsplit(getFileLocation(dataEntity), split = '.gz'))
   detach("package:R.utils", unload=TRUE)
   estReads = read.csv(x, row.names = 1)
-  colnames(estReads) = lookupRev$UDF.Investigator.Sample.Name[match(colnames(estReads), paste("X", lookupRev$Sample.Name,sep = ""))]
-  
+  lookupEnt = synGet('syn2770057')
+  lookup = read.csv(getFileLocation(lookupEnt))
+  colnames(estReads) = lookup$UDF.Investigator.Sample.Name[match(colnames(estReads), paste("X", lookup$Sample.Name,sep = ""))]
   data.dge = DGEList(counts=estReads,remove.zeros=TRUE)
-  
-  
-  # Remove clinical data for samples that don't have matching seq data.
-  noMatch = which(is.na(match(clinicalData$"Px_Code", colnames(data.dge))))
-  clinicalDataR = clinicalData[-noMatch,]
-  
-  # clean up date data
-  x = cleanUpDates(clinicalDataR[,8])
-  clinicalDataR[,8] = x
-  x = cleanUpDates(clinicalDataR[,9])
-  clinicalDataR[,9] = x
-  x = cleanUpDates(clinicalDataR[,12])
-  clinicalDataR[,12] = x
-  x = cleanUpDates(clinicalDataR[,13])
-  clinicalDataR[,13] = x
-  x = cleanUpDates(clinicalDataR[,14])
-  clinicalDataR[,14] = x
-  
-  return(list(DGE=data.dge, VARS=clinicalDataR))
+  return(data.dge)
 }
 
 
@@ -84,7 +67,7 @@ pcaCorrelationVal=function(x,y){cor.test(x,y)$estimate }
 pcaLM=function(x,y){summary(lm(x ~ y))$coefficients[2,4]}
 pcaLMEst=function(x,y){summary(lm(x ~ y))$coefficients[2,1]}
 
-calc_plot_PC_corr=function(in.pca,inClinical=clinicalDataR,categorical=c(),k=15){
+calc_plot_PC_corr=function(in.pca,inClinical,categorical=c(),k=15){
   # Using correlation against eigenvectors from all PALO data
   clinicalPCAcorrelations = matrix(NA, nrow = ncol(in.pca), ncol = ncol(inClinical))
   colnames(clinicalPCAcorrelations) = rep("default", ncol(clinicalPCAcorrelations))
@@ -108,30 +91,23 @@ calc_plot_PC_corr=function(in.pca,inClinical=clinicalDataR,categorical=c(),k=15)
   for (j in 1:length(categorical)) {
     temp[,j] = apply(in.pca,MARGIN=2,pcaLM,y=inClinical[,categorical[j]])
     colnames(temp)[j] = colnames(inClinical)[categorical[j]]
-    tempEst[,j] = apply(in.pca,MARGIN=2,pcaLMEst,y=inClinical[,categorical[j]])
-    colnames(tempEst)[j] = colnames(inClinical)[categorical[j]]
   }
   clinicalPCAcorrelations[,(ncol(clinicalPCAcorrelations)-length(categorical)+1):ncol(clinicalPCAcorrelations)] = temp
   colnames(clinicalPCAcorrelations)[(ncol(clinicalPCAcorrelations)-length(categorical)+1):ncol(clinicalPCAcorrelations)] = colnames(temp)
-  clinicalPCAcorrelationEst[,(ncol(clinicalPCAcorrelationEst)-length(categorical)+1):ncol(clinicalPCAcorrelationEst)] = tempEst
-  colnames(clinicalPCAcorrelationEst)[(ncol(clinicalPCAcorrelationEst)-length(categorical)+1):ncol(clinicalPCAcorrelationEst)] = colnames(tempEst)
   
   op = par(mai = c(3,1,1,1))
-  #  boxplot(clinicalPCAcorrelations[,1:26], las = 2)
   boxplot(clinicalPCAcorrelations, las = 2)
   par(op)
-  #  boxplot(t(clinicalPCAcorrelations), las = 2, main = "Correlation of each PCs vs each factor", ylab = "raw pvalue")
-  dotchart(t(clinicalPCAcorrelations[1:2,]), xlab = "pvalue", main = "Correlation with PC1 and PC2",bg="tomato",lcolor="tomato",cex=0.9)
   dotchart(t(clinicalPCAcorrelations[1:2,]), xlab = "pvalue", main = "Correlation with PC1 and PC2", xlim = range(0,0.1),bg="tomato",lcolor="tomato",cex=0.9)
   abline(v=0.05, lty = 2)
   abline(v = 0.01, lty = 3)
-  #apply(clinicalPCAcorrelations[1:20,],MARGIN=1,stripchart, las = 2)
   
   hist(clinicalPCAcorrelations)
   reds=brewer.pal(7,"Reds")
   dataFiltered = clinicalPCAcorrelations
   dataFiltered[which(clinicalPCAcorrelations > cutoff)] = NA
   heatmap(t(1-dataFiltered[1:k,]),Rowv = NA,Colv = NA,scale = "none",col=reds,margins = c(3,13))
+  heatmap(t(-log10(dataFiltered[1:k,])),Rowv = NA,Colv = NA,scale = "none",col=reds,margins = c(3,13))
   rdbu=brewer.pal(8,"RdBu")
   dataFiltered = clinicalPCAcorrelationEst
   dataFiltered[which(clinicalPCAcorrelations > cutoff)] = NA
