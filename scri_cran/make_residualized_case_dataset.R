ef87b8f6a44d9c9885a2362d3830fda447defb5a
kkdang <kristen.dang@sagebase.org>
2015-08-19 16:45:11 -0700

    New normalization that includes IGDD in model.

--- scri_cran/make_residualized_case_dataset.R
+++ scri_cran/make_residualized_case_dataset.R
@@ -1,7 +1,7 @@
 # ---
 # title: "make_residualized_case_dataset.R"
 # author: "Kristen Dang"
-# date: "Aug. 17, 2015"
+# date: "Aug. 19, 2015"
 # output: html_document
 # ---
 
@@ -29,7 +29,7 @@ metadataFiltered$Sample_Type[grep("Coronal",metadataFiltered$Sample_Type)] = "Co
 metadataFiltered$Sample_Type[grep("Metopic",metadataFiltered$Sample_Type)] = "Metopic"
 metadataFiltered$Sample_Type[grep("Sagittal",metadataFiltered$Sample_Type)] = "Sagittal"
 metadataFiltered$Sample_Type = as.factor(metadataFiltered$Sample_Type)
-minimalSetNoSex = c("Age_mos.","PCT_CORRECT_STRAND_READS","PCT_INTRONIC_BASES")
+minimalSetNoSex = c("Age_mos.","PCT_CORRECT_STRAND_READS","PCT_INTRONIC_BASES", "Initial_growth_duration_days")
 
 caseMeta = metadataFiltered[-which(metadataFiltered$Sample_Type == "Control"),]
 dim(caseMeta)
@@ -37,16 +37,15 @@ tempMeta = caseMeta[caseMeta$Px_Code %in% colnames(in.dge),]
 dim(tempMeta)
 
 ## Run model and get residuals
-### Using residuals of model that does NOT include AlkP or sample_type
 tempMeta$Sample_Type = as.factor(as.character(tempMeta$Sample_Type))
 tempModel = model.matrix(as.formula(paste("~",paste(minimalSetNoSex,collapse = "+"),sep = "")), data = tempMeta)
 
 data.voom = voom(in.dge,tempModel,plot=FALSE) 
 fit = lmFit(data.voom,tempModel)
-save(fit,file = "resid_cases_nosex_fit.Robj.bz2",compress = "bzip2")
+save(fit,file = "resid_cases_pIGDD_fit.Robj.bz2",compress = "bzip2")
 resid = residuals(fit,y = data.voom)
-write.table(formatC(resid,digits=6,format="fg"),file = "scri-cran_resid_cases_nosex.tsv",quote = FALSE,sep = "\t",row.names = TRUE,col.names = TRUE)
+write.table(formatC(resid,digits=6,format="fg"),file = "scri-cran_resid_cases_pIGDD.tsv",quote = FALSE,sep = "\t",row.names = TRUE,col.names = TRUE)
 
 ## Save data to Synapse
-synStore(File(path = "scri-cran_resid_cases_nosex.tsv",parentId='syn4889893'))
-synStore(File(path = "resid_cases_nosex_fit.Robj.bz2",parentId='syn4889893'))
+synStore(File(path = "scri-cran_resid_cases_pIGDD.tsv",parentId='syn4893931'))
+synStore(File(path = "resid_cases_pIGDD_fit.Robj.bz2",parentId='syn4893931'))
