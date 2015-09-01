81f155c26c1777d95b734fa15ffecb11e2d8493b
kkdang <kristen.dang@sagebase.org>
2015-09-01 13:07:00 -0700

    Modified for unmapped read upload.

--- cmc_code/copy_bam_to_synapse.py
+++ cmc_code/copy_bam_to_synapse.py
@@ -3,24 +3,25 @@ from synapseclient import File
 
 syn = synapseclient.login()
 
-# Make dictionary of filenames to load
 releasedFiles = dict()
 with open('/hpc/users/xdangk01/CMC_released_RNAseq') as released:
 	for line in released:
 		releasedFiles[line.strip()] = 1
-
-# Make dictionary of files already loaded		
+		
 loadedEntities = dict()
-results = syn.chunkedQuery(''.join(['SELECT id, name FROM entity WHERE parentId=="syn4645130"']))
+results = syn.chunkedQuery(''.join(['SELECT id, name FROM entity WHERE parentId=="syn4922873"']))
 for result in results:
 	loadedEntities[result['entity.name']] = result['entity.id']
 
-# Iterate through all files, loading those that are intended for release but are not yet loaded.
-with open('/sc/orga/projects/CommonMind/data/FROM_CORE/Phase1/BAMS.fofn') as bamFiles:
+
+with open('/sc/orga/projects/CommonMind/data/FROM_CORE/Phase1/UNMAPPED_BAMS.fofn') as bamFiles:
 	for line in bamFiles:
-		if os.path.basename(line.strip()).split('.')[0] in releasedFiles and not os.path.basename(line.strip()) in loadedEntities:
-			syn.store(File(path=line.strip(),parent='syn4645130',synapseStore=True))
-			print 'copied %s' % line.strip()
+		sampleName = line.strip().split('/')[11].lstrip('tophat_')
+		sampleFile = '.'.join([sampleName, 'unmapped.bam'])
+			
+		if sampleName in releasedFiles and not sampleFile in loadedEntities:
+			syn.store(File(path=line.strip(),parent='syn4922873',synapseStore=True,name=sampleFile))
+			print 'copied %s' % sampleFile
 		else:
-			print '%s' % line.strip()
+			print '%s' % sampleFile
 bamFiles.close()
