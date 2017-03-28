874290bdcf0dc45832e07c9062c5e7c8459aadd4
Kristen Dang <kristen.dang@gmail.com>
2017-03-28 10:24:52 -0700

    Adding picard data.

--- scri_cran/process_metadata_validation.R
+++ scri_cran/process_metadata_validation.R
@@ -6,37 +6,52 @@
 
 processMetadataVal=function(plot=TRUE){
   metadataTable = synTableQuery('SELECT * FROM syn7486948')
+  lookup = synTableQuery('SELECT * FROM syn7477113')
+  picardMetrics = read.delim(getFileLocation(synGet('syn8489352')),row.names = 1, header = TRUE,colClasses = c("character",rep("numeric",21)))
   
-  toFilter = c('Sample_ID', 'Order')
+  # join all
+  picardMetrics$sampName = sapply(rownames(picardMetrics),function(x) {unlist(strsplit(x,split = "[.]"))[1]})
+  combined = merge(as.data.frame(lookup@values),y = picardMetrics,by.x = "Sample_Name",by.y = "sampName")
+  
+  toFilter = c('Order', 'Space')
   metadataFiltered = metadataTable@values[,-which(colnames(metadataTable@values)%in%toFilter)]
-  rownames(metadataFiltered) = metadataTable@values$Sample_ID
+  allTogetherNow = merge(x=combined,y=metadataFiltered,by.x = "Investigator_Sample_Name",by.y = 'Sample_ID')
+  rm(combined)
   
-  metadataFiltered$Plate = as.factor(metadataFiltered$Plate)
-  metadataFiltered$Batch = as.factor(metadataFiltered$Batch)
-  metadataFiltered$Diagnosis = as.character(metadataFiltered$Diagnosis)
-  metadataFiltered$Diagnosis[grep("Coronal",metadataFiltered$Diagnosis)] = "Coronal"
-  metadataFiltered$Diagnosis[grep("Lambdoid",metadataFiltered$Diagnosis)] = "Lambdoid"
-  metadataFiltered$Diagnosis = as.factor(metadataFiltered$Diagnosis)
+  b = which(colnames(allTogetherNow) == "MEDIAN_5PRIME_TO_3PRIME_BIAS")
+  for (i in 3:b) {
+    allTogetherNow[,i] = as.numeric(as.character(allTogetherNow[,i]))}
+  
+  allTogetherNow$Plate = as.factor(allTogetherNow$Plate)
+  allTogetherNow$Batch = as.factor(allTogetherNow$Batch)
+  allTogetherNow$Diagnosis = as.character(allTogetherNow$Diagnosis)
+  allTogetherNow$Diagnosis[grep("Coronal",allTogetherNow$Diagnosis)] = "Coronal"
+  allTogetherNow$Diagnosis[grep("Lambdoid",allTogetherNow$Diagnosis)] = "Lambdoid"
+  allTogetherNow$Diagnosis = as.factor(allTogetherNow$Diagnosis)
   if (plot==TRUE) {
     
     op = par(mfrow = c(3,2))
-    hist(metadataFiltered$RNA_Days_to_Harvest)
-    hist(log(metadataFiltered$RNA_Days_to_Harvest))
-    metadataFiltered$RNA_Days_to_Harvest = log(metadataFiltered$RNA_Days_to_Harvest)
+    hist(allTogetherNow$RNA_Days_to_Harvest)
+    hist(log(allTogetherNow$RNA_Days_to_Harvest))
+    allTogetherNow$RNA_Days_to_Harvest = log(allTogetherNow$RNA_Days_to_Harvest)
     
-    pie(table(metadataFiltered$Plate), main = "Plate")
-    pie(table(metadataFiltered$Sex), main = "Sex")
-    dotchart(table(metadataFiltered$Batch), main = "Batch")
+    pie(table(allTogetherNow$Plate), main = "Plate")
+    pie(table(allTogetherNow$Sex), main = "Sex")
+    dotchart(table(allTogetherNow$Batch), main = "Batch")
     
-    pie(table(metadataFiltered$Diagnosis), main = "Diagnosis")
+    pie(table(allTogetherNow$Diagnosis), main = "Diagnosis")
     par(op)
-    dotchart(table(metadataFiltered$Diagnosis))
+    dotchart(table(allTogetherNow$Diagnosis))
     
-    pie(table(metadataFiltered$Diagnosis), main = "Diagnosis")
-    dotchart(table(metadataFiltered$Diagnosis))
+    pie(table(allTogetherNow$Diagnosis), main = "Diagnosis")
+    dotchart(table(allTogetherNow$Diagnosis))
     
-    
-    #corrgram(metadataFiltered,lower.panel = panel.pts,upper.panel = panel.pie)
+  
+    allTogetherNowShort = allTogetherNow[,-which(colnames(allTogetherNow) %in% c("RIBOSOMAL_BASES", "PCT_RIBOSOMAL_BASES"))]
+    c = which(colnames(allTogetherNowShort) == "INCORRECT_STRAND_READS")
+    for (i in 3:c) {
+      allTogetherNowShort[,i] = log(allTogetherNowShort[,i]) }
+    corrgram(allTogetherNowShort[,3:ncol(allTogetherNowShort)],lower.panel = panel.conf,upper.panel = panel.shade,type = "data",cor.method = "spearman", order = TRUE,label.srt = 40,cex.labels = 0.4)
   }
-  return(metadataFiltered)
+  return(allTogetherNow)
 }
